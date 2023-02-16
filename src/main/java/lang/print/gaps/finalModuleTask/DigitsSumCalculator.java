6892fd9e8eb83dd9ff13d4670cfee0d7f2f94d7f
kart <mkartveli9@gmail.com>
2023-02-16 23:31:37 +0000

    3final

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,14 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
+
     public void calculateSum(int number) {
+        int pirveli = number /1000;
+        int gardamaval = number - (pirveli*1000);
+        int meore = gardamaval /100;
+        gardamaval = gardamaval - (meore) * 100;
+        int mesame = gardamaval/10;
+        int bolo = number % 10;
+        System.out.println(pirveli + meore + mesame + bolo);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int aseul = number - (number / 100) * 100;
+        int shua  = (aseul/10) * 10;
+        int bolo = number/100;
+        int pirveli = (number%10) * 100;
+
+        System.out.println(pirveli +  shua + bolo);
+
     }
 }
